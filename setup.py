c2f3cf1b292b73cc77a60c31dc2eb5822555b779
Victor Gabriel <vgmduarte@gmail.com>
2020-08-20 00:32:01 -0300

    Setup initial commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="scitemplate",
+    version="0.1.dev0",
+    url="https://github.com/username/scitemplate",
+    author="Author Name",
+    author_email="author@domain.com",
+    description="Short Description",
+    packages=["scitemplate"],
+    install_requires=["numpy", "scipy"],
+)
