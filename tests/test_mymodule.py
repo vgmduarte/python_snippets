320b017f999ef05dd925465c6a39f321088a6928
Victor Gabriel <vgmduarte@gmail.com>
2020-08-20 17:18:33 -0300

    Added tests folder

---
+++ tests/test_mymodule.py
@@ -0,0 +1,10 @@
+import pytest as pt
+import numpy as np
+import numpy.testing as npt
+
+from vicpy.mymodule import myfunc
+
+def test_myfunc():
+    npt.assert_array_equal(myfunc(3), np.array([]))
+    npt.assert_array_eq
+    
