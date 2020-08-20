f19151e721761a755d27eca0ac71df86a0012f94
Victor Gabriel <vgmduarte@gmail.com>
2020-08-20 17:17:39 -0300

    Updated setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="scitemplate",
+    name="vicpy",
     version="0.1.dev0",
-    url="https://github.com/username/scitemplate",
+    url="https://github.com/username/vicpy",
     author="Author Name",
     author_email="author@domain.com",
     description="Short Description",
-    packages=["scitemplate"],
-    install_requires=["numpy", "scipy"],
+    packages=["vicpy"],
+    install_requires=["numpy"],
 )
